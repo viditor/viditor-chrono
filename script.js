3cd9ea8e2ec008873a10aaa04f10a90020175573
Arcym <andrewmcp333@gmail.com>
2013-02-12 10:01:01 -0500

    Binded the rightarrowkey to move the asset a bit.
    I defined a couple of functions to assist in processing the position of
    the asset, by parsing the pixely suffix from the string or by returning
    the element from the page.

--- script.js
+++ script.js
@@ -8,6 +8,18 @@ var asset =
 window.addEventListener("load", function(event)
 {
 	document.getElementById("&2").innerHTML += '<asset id="@' + asset.idnum + '"></asset>';
-	document.getElementById("@" + asset.idnum).style.backgroundImage = "url('assets/" + asset.filename + ".png')";
-	document.getElementById("@" + asset.idnum).style.left = asset.position + "px";
+	accessAsset(asset.idnum).style.backgroundImage = "url('assets/" + asset.filename + ".png')";
+	accessAsset(asset.idnum).style.left = asset.position + "px";
 }, false);
+
+window.addEventListener("keydown", function(event)
+{
+	if(event.keyCode == 39)
+	{
+		var position = parsePix(accessAsset(asset.idnum).style.left);
+		accessAsset(asset.idnum).style.left = position + 1 + "px";
+	}
+}, false);
+
+function parsePix(string) {return parseInt(string.slice(0,-2));}
+function accessAsset(idnum) {return document.getElementById("@" + idnum);}
