0bc433cb752ec729c62b721d1cdc17d5382350f5
Arcym <andrewmcp333@gmail.com>
2013-02-14 04:54:03 -0500

    Defined a function for binding the keystrokes.

--- kbdin.js
+++ kbdin.js
@@ -1,14 +1,34 @@
 var shifting = false;
 
+var kbdin = 
+{
+	binded: new Object(),
+	
+	bindKeystroke: function(keycode, functionality)
+	{
+		this.binded[keycode] = functionality;
+	},
+	
+	isBinded: function(keycode)
+	{
+		return this.binded[keycode];
+	}
+}
+
 window.addEventListener("keydown", function(event)
 {
-	if(event.keyCode == 39) {if(shifting) {moveAsset(RIGHT, JUMP); return;} else {moveAsset(RIGHT, MOVE);} return;}
-	else if(event.keyCode == 37) {if(shifting) {moveAsset(LEFT, JUMP); return;} else {moveAsset(LEFT, MOVE);} return;}
-	else if(event.keyCode == 16) {shifting = true; log("Jumpmode"); return;}
-	else if(event.keyCode == 32) {shiftCursor(RIGHT); return;}
+	if(kbdin.isBinded(event.keyCode))
+	{
+		kbdin.binded[event.keyCode]();
+	}
 }, false);
 
 window.addEventListener("keyup", function(event)
 {
 	if(event.keyCode == 16) {shifting = false; log("");}
 }, false);
+
+kbdin.bindKeystroke(39, function() {if(shifting) {moveAsset(RIGHT, JUMP); return;} else {moveAsset(RIGHT, MOVE);}});
+kbdin.bindKeystroke(37, function() {if(shifting) {moveAsset(LEFT, JUMP); return;} else {moveAsset(LEFT, MOVE);}});
+kbdin.bindKeystroke(16, function() {shifting = true; log("Jumpmode");});
+kbdin.bindKeystroke(32, function() {shiftCursor(RIGHT); return;});
