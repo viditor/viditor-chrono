6c6d8998b95f75a0c9073903f5b20b4a822aa084
Arcym <andrewmcp333@gmail.com>
2013-02-14 05:18:52 -0500

    Fixed the interrupting keyevents through hashing.

--- kbdin.js
+++ kbdin.js
@@ -2,6 +2,7 @@ var shifting = false;
 
 var kbdin = 
 {
+	stroked: new Object(),
 	upbinded: new Object(),
 	downbinded: new Object(),
 	
@@ -25,25 +26,49 @@ var kbdin =
 		return this.upbinded[keycode];
 	},
 	
+	isStroked: function(keycode)
+	{
+		return this.stroked[keycode];
+	},
+	
 	onDownstroke: function(keycode)
 	{
-		if(this.isDownbinded(keycode))
+		if(!this.isStroked(keycode))
 		{
-			this.downbinded[keycode]();
+			this.stroked[keycode] = 1;
+			
+			if(this.isDownbinded(keycode))
+			{
+				this.downbinded[keycode]();
+			}
 		}
 	},
 	
 	onUpstroke: function(keycode)
 	{
+		delete this.stroked[keycode];
+		
 		if(this.isUpbinded(keycode))
 		{
 			this.upbinded[keycode]();
+		}
+	},
+	
+	handleKeystrokes: function()
+	{
+		for(var keycode in this.downbinded)
+		{
+			if(this.isStroked(keycode))
+			{
+				this.downbinded[keycode]();
+			}
 		}
 	}
 }
 
 window.addEventListener("keydown", function(event) {kbdin.onDownstroke(event.keyCode);}, false);
 window.addEventListener("keyup", function(event) {kbdin.onUpstroke(event.keyCode);}, false);
+window.setInterval(function() {kbdin.handleKeystrokes();}, 50);
 
 kbdin.downbindKeystroke(39, function() {if(shifting) {moveAsset(RIGHT, JUMP); return;} else {moveAsset(RIGHT, MOVE);}});
 kbdin.downbindKeystroke(37, function() {if(shifting) {moveAsset(LEFT, JUMP); return;} else {moveAsset(LEFT, MOVE);}});
