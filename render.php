1bf1b27d46913bef21423f34bb491c0bfcdb81e4
Andrew McPherson <andrewmcp333@gmail.com>
2013-02-19 11:02:24 -0500

    Compiled together the video through FFMPEG.

--- render.php
+++ render.php
@@ -8,5 +8,8 @@ for($num = 0; $num < count($result); $num++)
 	$rendering = $rendering . "assets/" . $result[$num]->filename . ".mpg ";
 }
 
-echo $rendering;
+$rendered = 'assets/' . date("mdYhis") . '.mpg';
+exec('cat ' . $rendering . '| ffmpeg -i - ' . $rendered);
+
+echo $rendered;
 ?>
