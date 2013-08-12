2ace9b0de44cc4257be6cd8c47724b089dd1620d
Arcym <andrewmcp333@gmail.com>
2013-08-12 06:36:58 -0400

    Recorded coordinates while repositioning assets.

--- viditor.script.js
+++ viditor.script.js
@@ -1,8 +1,17 @@
+var undo = [];
+
 $(function()
 {
 	$(".asset").draggable(
 	{
 		grid: [16, 0],
-		stack: ".asset"
+		stack: ".asset",
+		stop: function(event, element)
+		{
+			repositioning = {};
+			repositioning.horizposition = Math.ceil(element.originalPosition.left / 16) * 16;
+			repositioning.instantiationidnum = $(this).attr("id");
+			undo.push(repositioning);
+		}
 	});
 });
