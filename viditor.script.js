709cdf7be2649b052ecb652a8742734394914b05
Arcym <andrewmcp333@gmail.com>
2013-08-12 08:15:27 -0400

    Extended the code for undoing to include redoing.

--- viditor.script.js
+++ viditor.script.js
@@ -1,4 +1,5 @@
 var undo = [];
+var redo = [];
 
 $(function()
 {
@@ -22,10 +23,35 @@ $(function()
 			if(undo.length > 0)
 			{
 				toundo = undo.pop();
+				
+				toredo = {};
+				toredo.instantiationidnum = toundo.instantiationidnum;
+				
 				if(toundo.horizposition != null)
 				{
+					toredo.horizposition = Math.floor($("#" + toundo.instantiationidnum).position().left / 16 + 0.5);
 					$("#" + toundo.instantiationidnum).css("left", toundo.horizposition * 16);
 				}
+				
+				redo.push(toredo);
+			}
+		}
+		else if(event.keyCode == 89 && event.ctrlKey)
+		{
+			if(redo.length > 0)
+			{
+				toredo = redo.pop();
+				
+				toundo = {};
+				toundo.instantiationidnum = toredo.instantiationidnum;
+				
+				if(toredo.horizposition != null)
+				{
+					toundo.horizposition = Math.floor($("#" + toredo.instantiationidnum).position().left / 16 + 0.5);
+					$("#" + toredo.instantiationidnum).css("left", toredo.horizposition * 16);
+				}
+				
+				undo.push(toundo);
 			}
 		}
 	});
