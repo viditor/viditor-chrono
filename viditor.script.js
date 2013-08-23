e48615c747bd684639cde264c74fa270943b97e6
Arcym <andrewmcp333@gmail.com>
2013-08-23 17:15:04 -0400

    Scripted the dragging of assets into the timeline.

--- viditor.script.js
+++ viditor.script.js
@@ -2,11 +2,43 @@ var PIXELS_PER_TIMEBIT = 20;
 
 $(function()
 {
-	$(".timeline > .asset").draggable(
+	$(".explorer > .asset").draggable(
+	{
+		scroll: false,
+		stack: ".asset",
+		helper: "clone",
+		revert: "invalid"
+	});
+	
+	$(".timeline").droppable(
+	{
+		accept: ".explorer > .asset",
+		drop: function(event, element)
+		{
+			data = new Object();
+			data.instanceidnum = new Date().getTime();
+			data.horizposition = Math.floor((element.position.left / PIXELS_PER_TIMEBIT) + 0.5);
+			data.color = $(element.draggable).css("background-color");
+			
+			instantiateAsset(data);
+		}
+	});
+});
+
+function instantiateAsset(data)
+{
+	$asset = $("<div></div>");
+	$asset.attr("class", "asset");
+	$asset.attr("id", data.instanceidnum);
+	$asset.css("left", data.horizposition * PIXELS_PER_TIMEBIT);
+	$asset.css("position", "absolute");
+	$asset.css("background-color", data.color);
+
+	$asset.draggable(
 	{
 		stack: ".asset",
 		grid: [PIXELS_PER_TIMEBIT, 0]
 	});
-	
-	$(".timeline > .asset").css("position", "absolute");
-});
+
+	$(".timeline").append($asset);
+}
