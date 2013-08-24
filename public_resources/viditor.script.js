e941db255993cbe0df877a029ea8f80deb2dd56a
Arcym <andrewmcp333@gmail.com>
2013-08-24 06:57:16 -0400

    Configured the server to listen for messages.

--- public_resources/viditor.script.js
+++ public_resources/viditor.script.js
@@ -1,4 +1,16 @@
+var socket = io.connect("http://127.0.0.1");
+
 var PIXELS_PER_TIMEBIT = 20;
+
+socket.on("instantiate asset", function(data)
+{
+	instantiateAsset(data);
+});
+
+socket.on("update asset", function(data)
+{
+	updateAsset(data);
+});
 
 $(function()
 {
@@ -16,11 +28,11 @@ $(function()
 		drop: function(event, element)
 		{
 			data = new Object();
-			data.instanceidnum = new Date().getTime();
+			data.instantiationidnum = new Date().getTime();
 			data.horizposition = Math.floor((element.position.left / PIXELS_PER_TIMEBIT) + 0.5);
 			data.color = $(element.draggable).css("background-color");
 			
-			instantiateAsset(data);
+			socket.emit("instantiate asset", data);
 		}
 	});
 });
@@ -29,7 +41,7 @@ function instantiateAsset(data)
 {
 	$asset = $("<div></div>");
 	$asset.attr("class", "asset");
-	$asset.attr("id", data.instanceidnum);
+	$asset.attr("id", data.instantiationidnum);
 	$asset.css("left", data.horizposition * PIXELS_PER_TIMEBIT);
 	$asset.css("position", "absolute");
 	$asset.css("background-color", data.color);
--- viditor.server.js
+++ viditor.server.js
@@ -24,6 +24,15 @@ app.get("/", function(request, response)
 
 app.use(express.static(__dirname + "/public_resources"));
 
+io.sockets.on("connection", function(socket)
+{
+	socket.on("instantiate asset", function(data)
+	{
+		console.log("INSTANTIATE ASSET :: " + JSON.stringify(data));
+		io.sockets.emit("instantiate asset", data);
+	});
+});
+
 server.listen(3000);
 
 console.log("Server is running on 127.0.0.1:3000");
