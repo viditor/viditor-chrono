354b979c3f8c551f5bec04b4ea4d03c583529870
Andrew McPherson <andrewmcp333@gmail.com>
2014-05-27 15:51:07 -0400

    Refactored the dumb orsuts to boring instances.

--- viditor.timeline.js
+++ viditor.timeline.js
@@ -1,15 +1,23 @@
-var Orsuts = new Meteor.Collection("orsuts");
+var Instances = new Meteor.Collection("instances");
 
 if(Meteor.isClient)
 {
-	Template.track.orsuts = Orsuts.find();
+	Template.track.instances = Instances.find();
+	
+	Template.track.events =
+	{
+		"click .instance": function()
+		{
+			Instances.update(this._id, {$inc: {position: 100}});
+		}
+	}
 }
 
 if(Meteor.isServer)
 {
 	Meteor.startup(function()
 	{
-		Orsuts.remove({});
-		Orsuts.insert({handle: "snowing", position: 100, length: 100});
+		Instances.remove({});
+		Instances.insert({handle: "snowing", position: 100, length: 100});
 	});
 }
