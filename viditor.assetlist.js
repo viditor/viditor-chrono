536eb44817be1469f66dbe9b412ba3dd7ea2f9c0
Alfonso <alfonso@agarzola.com>
2014-06-11 16:29:45 -0400

    Add expand/collapse button to assetlist
    Includes JS event handling. Surely there are better/more efficient ways of handling this. I’ll leave that to the experts. This illustrates what classes I need to change/add.

--- viditor.assetlist.js
+++ viditor.assetlist.js
@@ -12,6 +12,20 @@ if(Meteor.isClient)
 		"click .asset": function()
 		{
 			var _id = Instances.insert({asset: this._id, handle: this.handle, length: this.length, position: 0, endposition: this.length, track: 1});
+		},
+		"click .assetlist-expand": function()
+		{
+			var button = document.getElementById("assetlist-expand-collapse");
+			var al = document.getElementById("assetlist");
+			button.className = "assetlist-collapse";
+			al.className = al.className + " expanded";
+		},
+		"click .assetlist-collapse": function()
+		{
+			var button = document.getElementById("assetlist-expand-collapse");
+			var al = document.getElementById("assetlist");
+			button.className = "assetlist-expand";
+			al.className = al.className.replace(" expanded", "");
 		}
 	}
 }
