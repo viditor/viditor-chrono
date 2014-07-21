ac9cdd39fc3e79e0d6093824ad0517d12d730c15
arcym <andrewmcp333@gmail.com>
2014-07-21 16:19:37 -0400

    Put together some styles for the dashboard.

--- viditor.dashboard.js
+++ viditor.dashboard.js
@@ -11,4 +11,22 @@ if(Meteor.isClient)
 			}
 		];
 	}
+	
+	Template.dashboard.selection = function()
+	{
+		return Session.get("selection");
+	}
+	
+	Template.dashboard.events =
+	{
+		"click li": function(event)
+		{
+			event.preventDefault();
+			Session.set("selection", this);
+		},
+		"click #abort-preview": function(event)
+		{
+			Session.set("selection", undefined);
+		}
+	}
 }
